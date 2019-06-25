d0a4818ccdbb7bba0538e90922f979694e739558
ningfei.wang@hiroop.com <ningfei.wang@hiroop.com>
2019-06-25 19:12:11 +0800

    登录页
    登录页

---
+++ shop/src/views/Login.vue
@@ -0,0 +1,63 @@
+<template>
+  <cube-form :model="model" :schema="schema" @validate="validateHandler" @submit="submitHandler"></cube-form>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      model: {
+        username: "",
+        password: ""
+      },
+      schema: {
+        fields: [
+          {
+            type: "input",
+            modelKey: "username",
+            label: "用户名",
+            props: {
+              placeholder: "请输入用户名"
+            },
+            rules: {
+              required: true
+            },
+            messages: {
+              required: "请输入正确的用户名"
+            },
+            trigger: "blur"
+          },
+          {
+            type: "input",
+            modelKey: "password",
+            label: "密码",
+            props: {
+              placeholder: "请输入密码",
+              type: "password",
+              eye: true
+            },
+            rules: {
+              required: true
+            },
+            messages: {
+              required: "请输入正确的密码"
+            },
+            trigger: "blur"
+          },
+          {
+            type: "button",
+            label: "登陆"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    validateHandler() {
+      console.log("校验");
+    },
+    submitHandler() {
+      event.stopPropagation();
+    }
+  }
+};
+</script>
---
+++ shop/vue.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  devServer: {
+    before(app) {
+      app.get("/api/login/", (req, res) => {
+        res.json({
+          code: 0,
+          token: "jilei",
+          message: "登陆成功"
+        });
+      });
+    }
+  },
+  css: {
+    loaderOptions: {
+      stylus: {
+        "resolve url": true,
+        import: []
+      }
+    }
+  },
+  pluginOptions: {
+    "cube-ui": {
+      postCompile: true,
+      theme: false
+    }
+  }
+};
